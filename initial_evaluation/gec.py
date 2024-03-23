6b35d62208e85f5092dc79adad00d020767dbdf9
David Samuel <david.samuel@seznam.cz>
2024-03-23 17:31:05 +0100

    add warning about ERRANT

--- initial_evaluation/gec.py
+++ initial_evaluation/gec.py
@@ -204,4 +204,5 @@ def main():
 
 
 if __name__ == "__main__":
+    print("\nWARNING: make sure you have ERRANT installed to run the evaluation! Available here: https://github.com/chrisjbryant/errant\n\n", flush=True)
     main()
